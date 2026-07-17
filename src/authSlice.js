36b85269c8bb15a64f14c51e5a0ccf0efe5dd1dc
Keshav Thakur <keshavt043@gmail.com>
2026-07-17 10:25:39 +0530

    Update17-7-26

---
+++ src/authSlice.js
@@ -0,0 +1,133 @@
+import {createAsyncThunk,createSlice} from "@reduxjs/toolkit";
+import axiosClient from "../src/utils/axiosClient.js";
+
+const registerUser=createAsyncThunk(
+    "auth/register",
+    async (userData, {rejectWithValue})=>{
+        try{
+        const response=await axiosClient.post("/user/register",userData);
+        return response.data.result;
+        }
+        catch(err){
+           return rejectWithValue(err);
+        }
+    }
+);
+
+const loginUser=createAsyncThunk(
+    "auth/login",
+    async (userData, {rejectWithValue})=>{
+        try{
+        const response=await axiosClient.post("/user/login", userData);
+        return response.data.result;
+        }
+        catch(err){
+            return rejectWithValue(err);
+        }
+    }
+);
+
+const authenticateUser=createAsyncThunk(
+    "auth/authenticate",
+    async (_, {rejectWithValue})=>{
+        try{
+            const {data}=await axiosClient.get("/user/authenticate");
+            return data.result;
+        }
+        catch(err){
+            return rejectWithValue(err);
+        }
+    }
+);
+
+const logoutUser=createAsyncThunk(
+    "/auth/logout",
+    async (_, {rejectWithValue})=>{
+        try{
+            await axiosClient.get("/user/logout");
+            return null;
+        }
+        catch(err){
+            return rejectWithValue(err);
+        }
+    }
+);
+
+const authSlice=createSlice({
+    name:"auth",
+    initialState:{
+        user:null,
+        isAuthenticate:false,
+        loading:false,
+        error:null
+    },
+    reducers:{},
+    extraReducers:(builder)=>{
+        builder.addCase(registerUser.pending, (state)=>{
+            state.loading=true,
+            state.error=false
+        })
+        .addCase(registerUser.fulfilled, (state,action)=>{
+            state.user=action.payload,
+            state.isAuthenticate=true,
+            state.loading=false
+        })
+        .addCase(registerUser.rejected, (state,action)=>{
+            state.error=action.payload?.message || "Something went wrong...",
+            state.loading=false,
+            state.isAuthenticate=false,
+            state.user=null
+        })
+
+        .addCase(loginUser.pending, (state)=>{
+            state.loading=true,
+            state.error=false
+        })
+        .addCase(loginUser.fulfilled, (state,action)=>{
+            state.user=action.payload,
+            state.isAuthenticate=true,
+            state.loading=false
+        })
+        .addCase(loginUser.rejected , (state,action)=>{
+            state.error=action.payload?.message || "Something went wrong...",
+            state.loading=false,
+            state.user=null,
+            state.isAuthenticate=false
+        })
+
+
+        .addCase(authenticateUser.pending, (state)=>{
+            state.loading=true,
+            state.error=false
+        })
+        .addCase(authenticateUser.fulfilled, (state,action)=>{
+            state.user=action.payload,
+            state.isAuthenticate=true,
+            state.loading=false
+        })
+        .addCase(authenticateUser.rejected , (state,action)=>{
+            state.error=action.payload?.message || "Something went wrong...",
+            state.loading=false,
+            state.user=null,
+            state.isAuthenticate=false
+        })
+
+        .addCase(logoutUser.pending, (state)=>{
+            state.loading=true,
+            state.error=false
+        })
+        .addCase(logoutUser.fulfilled ,(state)=>{
+            state.isAuthenticate=false,
+            state.loading=false,
+            state.user=nul
+        })
+        .addCase(logoutUser.rejected , (state,action)=>{
+            state.error=action.payload?.message || "Something went wrong...",
+            state.loading=false,
+            state.user=null,
+            state.isAuthenticate=false
+        });
+    }
+});
+
+export default authSlice.reducer;
